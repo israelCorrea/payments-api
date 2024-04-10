da1d2359c83926b53874691d694ecdee73aabb73
israelCorrea <emerick.correa@gmail.com>
2024-04-10 16:34:24 -0300

    Ativacao de Async no projeto e uso do Async no metodo.

--- src/main/java/br/com/bank/payments/PaymentsApplication.java
+++ src/main/java/br/com/bank/payments/PaymentsApplication.java
@@ -4,9 +4,11 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "Payments API", version = "1.0.0", description = "API desenvolvida para administrar Pagamentos e oferecer recursos para outros sistemas Incluir, Consultar, Atualizar, Atualizar Parcialmente e Deletar os Pagamentos."))
+@EnableAsync
 public class PaymentsApplication {
 
 	public static void main(String[] args) {
